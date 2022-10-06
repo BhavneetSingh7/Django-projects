b860db5f04e9d13f11f72a39dff2858b05662e32
Bhavneet Singh <97750271+BhavneetSingh7@users.noreply.github.com>
2022-10-06 23:29:09 +0530

    Delete newsletter directory

--- newsletter/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-from . models import *
-
-class CredsForm(ModelForm):
-
-    class Meta :
-        model = Creds
-        fields = '__all__'
--- newsletter/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'newsletter'
-urlpatterns = [
-    path('',views.index,name='newsletter'),
-    path('check/',views.check,name='newsletter_check'),
-    path('<str:subscriber>/<str:verify_key>/',views.verification,name='newsletter_verify'),
-]
