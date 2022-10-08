262f014c5acf362215733926ffd2d9d552d38905
Bhavneet Singh <97750271+BhavneetSingh7@users.noreply.github.com>
2022-10-08 20:08:24 +0530

    Update urls.py

--- usecase/newsletter/urls.py
+++ usecase/newsletter/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('',views.index,name='newsletter'),
     path('check/',views.check,name='newsletter_check'),
     path('<str:subscriber>/<str:verify_key>/',views.verification,name='newsletter_verify'),
+    path('<str:subscriber>/<str:verify_key>/unsubscribe/',views.unsubscribe,name='newsletter_unsubscribe'),
 ]
